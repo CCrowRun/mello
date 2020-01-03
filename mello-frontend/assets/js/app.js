3cd438d7c7c2f49714197b958c80fb5be39db687
C Crow <carrie.g.crow@gmail.com>
2020-01-03 14:31:12 -0600

    Create lists dynamically
    Created app.js to add elements to the DOM

---
+++ mello-frontend/assets/js/app.js
@@ -0,0 +1,20 @@
+var $boardContainer = document.querySelector('.container');
+
+var board = new Board();
+
+board.addList('To Do');
+board.addList('In Progress');
+board.addList('Done');
+
+board.lists.forEach(function(list, index) {
+	var $listContainer = document.createElement('div');
+	$listContainer.className = 'list';
+
+	var $header = document.createElement('header');
+	var $headerButton = document.createElement('button');
+	$headerButton.textContent = list.title;
+
+	$header.appendChild($headerButton);
+	$listContainer.appendChild($header);
+	$boardContainer.appendChild($listContainer);
+});
